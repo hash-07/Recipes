1a9d035ccd4a68bc1cec4c3f2e5ead7f929bb8a7
hasheer <hasheer11@gmail.com>
2020-07-21 18:28:25 +0530

    Updates

---
+++ precache-manifest.a771a4e414f68c62811f7ef9e2c6d4bb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a98b81d9e6b54a6d9053edc2fcb4f54",
+    "url": "/Recipes/index.html"
+  },
+  {
+    "revision": "df3a6075d8452c5f7755",
+    "url": "/Recipes/static/css/main.77d167ba.chunk.css"
+  },
+  {
+    "revision": "a728d8a7887bf493653b",
+    "url": "/Recipes/static/js/2.64ad2dab.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Recipes/static/js/2.64ad2dab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df3a6075d8452c5f7755",
+    "url": "/Recipes/static/js/main.519d8da0.chunk.js"
+  },
+  {
+    "revision": "ad90e31245b7caeed1c5",
+    "url": "/Recipes/static/js/runtime-main.e5ec55a4.js"
+  }
+]);
